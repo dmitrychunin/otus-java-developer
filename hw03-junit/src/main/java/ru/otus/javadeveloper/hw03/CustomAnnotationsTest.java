60d5e7dc0aeaaf859bd3b19cdabb8544bf148860
dmitry <chunindi@global.bcs.ru>
2019-05-01 18:52:31 +0300

    custom junit annotation

---
+++ hw03-junit/src/main/java/ru/otus/javadeveloper/hw03/CustomAnnotationsTest.java
@@ -0,0 +1,49 @@
+package ru.otus.javadeveloper.hw03;
+
+import ru.otus.javadeveloper.hw03.annotation.*;
+
+class CustomAnnotationsTest {
+    CustomAnnotationsTest() {
+        System.out.println("Call of the constructor");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("BeforeAll");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("AfterAll");
+    }
+
+    @BeforeEach
+    void beforeEach3() {
+        System.out.println("BeforeEach3");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("BeforeEach");
+    }
+
+    @BeforeEach
+    void beforeEach2() {
+        System.out.println("BeforeEach2");
+    }
+
+    @Test
+    void testOne() {
+        System.out.println("testOne");
+    }
+
+    @Test
+    void testTwo() {
+        System.out.println("testTwo");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("AfterEach");
+    }
+}
