4055a9c2be53a37911488d35f03079a220dc9934
dmitry <chunindi@global.bcs.ru>
2019-06-29 15:52:20 +0300

    delete CheckableAndRecoverable

--- hw06-atm/src/main/java/ru/otus/javadeveloper/hw06/atm/core/atm/CheckableAndRecoverable.java
+++
@@ -1,4 +0,0 @@
-package ru.otus.javadeveloper.hw06.atm.core.atm;
-
-public interface CheckableAndRecoverable extends CheckableBalance, ManuallyRecoverable {
-}
--- hw06-atm/src/main/java/ru/otus/javadeveloper/hw06/atm/core/atm/Department.java
+++ hw06-atm/src/main/java/ru/otus/javadeveloper/hw06/atm/core/atm/Department.java
@@ -6,24 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
-public class Department implements CheckableAndRecoverable {
-    private List<CheckableAndRecoverable> atmList = new ArrayList<>();
+public class Department implements CheckableBalance, ManuallyRecoverable {
+    private List<Object> atmList = new ArrayList<>();
 
-    public void removeObserver(CheckableAndRecoverable atm) {
+    public <T extends CheckableBalance & ManuallyRecoverable> void removeObserver(T atm) {
         atmList.remove(atm);
     }
 
-    public void addObserver(CheckableAndRecoverable atm) {
+    public <T extends CheckableBalance & ManuallyRecoverable> void addObserver(T atm) {
         atmList.add(atm);
     }
 
     @Override
     public long getBalance() {
-        return atmList.stream().map(CheckableAndRecoverable::getBalance).reduce(0L, Long::sum);
+        return atmList.stream().map(atm -> ((CheckableBalance) atm).getBalance()).reduce(0L, Long::sum);
     }
 
     @Override
     public void recover() {
-        atmList.forEach(CheckableAndRecoverable::recover);
+        atmList.forEach(atm -> ((ManuallyRecoverable) atm).recover());
     }
 }
