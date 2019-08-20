1c611081b569859b7ecd1a58835fa3f10edc9f2e
Dmitry <chunindi@bcs.ru>
2019-08-20 14:22:38 +0300

    fix reviewer issues2

---
+++ hw15-message-system/src/main/java/ru/otus/javadeveloper/hw15/BackendConfig.java
@@ -0,0 +1,13 @@
+package ru.otus.javadeveloper.hw15;
+
+import org.springframework.context.annotation.Configuration;
+import ru.otus.javadeveloper.hw15.backend.executor.DbExecutorHibernate;
+import ru.otus.javadeveloper.hw15.backend.executor.DbHibernateExecutorHibernateImpl;
+import ru.otus.javadeveloper.hw15.backend.model.User;
+
+@Configuration
+public class BackendConfig {
+    public DbExecutorHibernate<User> dbService() {
+        return new DbHibernateExecutorHibernateImpl<>();
+    }
+}
